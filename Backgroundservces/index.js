e5ea9c07c0f0565ec2a49f90e2b4761140e6c662
Samuraxworld <Samuraxworld@gmail.com>
2024-10-23 22:38:32 +0100

    Completed background Services for Email responder

--- Backgroundservces/index.js
+++ Backgroundservces/index.js
@@ -3,21 +3,26 @@ const cron = require("node-cron");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const expenseEmail = require("./EmailService/Expense");
 
 dotenv.config();
 
-mongoose.connect(process.env.DB_CONNECTION).then(() => {
-    console.log("DB Connection is successful")
-}).catch((err) => {
-    console.log(err)
-});
+mongoose
+  .connect(process.env.DB_CONNECTION)
+  .then(() => {
+    console.log("DB Connection is successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const schedule = () =>{
-    cron.schedule('* * * * * *', () => {
-        console.log('running a task every second');
-      });
-}
+const schedule = () => {
+  cron.schedule("* * * * * *", () => {
+    expenseEmail();
+  });
+};
+
 schedule();
-app.listen(process.env.PORT,() => {
-    console.log('server is running on port ${process.env.PORT}')
-})
+app.listen(process.env.PORT, () => {
+  console.log("server is running on port ${process.env.PORT}");
+});
