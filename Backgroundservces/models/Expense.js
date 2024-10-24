02a8b5ba063d0617b31632ea5a470d5f15d89509
Samuel Ugochukwu <Samuraxworld@gmail.com>
2024-10-24 08:58:07 +0100

    Delete Backgroundservces directory

--- Backgroundservces/models/Expense.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const ExpenseSchema = mongoose.Schema({
-  label: { type: String, require: true },
-  value: { type: Number, require: true },
-  date: { type: String, require: true },
-});
-
-module.exports = mongoose.model("Expense", ExpenseSchema);
