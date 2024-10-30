e05cea61f7ba7c59e1e5aea72024f88595288495
Mogbolahan Folarin <mogbolahan@aol.com>
2024-10-30 23:20:19 +0100

    background normalized

--- expensewise/src/Pages/Home/Homepage.js
+++ expensewise/src/Pages/Home/Homepage.js
@@ -10,15 +10,15 @@ function Homepage() {
             <header className='header'>
                 <a href='/'><img src={logo} alt='ExpenseWise Logo' /></a>
                 <ul className='nav'>
-                    <li><a href='/Login'><i className="fas fa-sign-in-alt"></i> Login</a></li>
-                    <li><a href='/aboutus'><i className='fas fa-info-circle'></i>About Us</a></li>
-                    <li><a href='/contactus'><i className='fas fa-envelope'></i>Contact Us</a></li>
-                    <li><a href='/logout'><i className='fas fa-sign-out-alt'></i>Log Out</a></li>
+                    <li><a href='/login'><i className="fas fa-sign-in-alt"></i> Login</a></li>
+                    <li><a href='/aboutus'><i className='fas fa-info-circle'></i> About Us</a></li>
+                    <li><a href='/contactus'><i className='fas fa-envelope'></i> Contact Us</a></li>
+                    <li><a href='/logout'><i className='fas fa-sign-out-alt'></i> Log Out</a></li>
                 </ul>
             </header>
             <section className='main section'>
                 <img className='img-bg' src={page} alt="Background" />
-                <h1>Welcome! <br />Your reliable App for tracking expenses...</h1>
+                <h1>Welcome! <br />Your reliable app for tracking expenses...</h1>
             </section>
             <footer className='footer'>
                 <p>&copy; 2024 ExpenseWise. All rights reserved.</p>
