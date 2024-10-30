3e70e3324631e07cfb9a445398045fb50dfe28f3
Mogbolahan Folarin <mogbolahan@aol.com>
2024-10-30 23:43:47 +0100

    fluid transition between homepage and login page

--- expensewise/src/Pages/Home/Homepage.js
+++ expensewise/src/Pages/Home/Homepage.js
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Homepage.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import logo from '../../images/logo.png';
 import page from '../../images/background-img.png';
 
 function Homepage() {
+    const [showLogin, setShowLogin] = useState(false);
+
+    const handleShowLogin = () => setShowLogin(true);
+
     return (
         <>
-            <header className='header'>
-                <a href='/'><img src={logo} alt='ExpenseWise Logo' /></a>
-                <ul className='nav'>
-                    <li><a href='/login'><i className="fas fa-sign-in-alt"></i> Login</a></li>
-                    <li><a href='/aboutus'><i className='fas fa-info-circle'></i> About Us</a></li>
-                    <li><a href='/contactus'><i className='fas fa-envelope'></i> Contact Us</a></li>
-                    <li><a href='/logout'><i className='fas fa-sign-out-alt'></i> Log Out</a></li>
-                </ul>
-            </header>
-            <section className='main section'>
-                <img className='img-bg' src={page} alt="Background" />
-                <h1>Welcome! <br />Your reliable app for tracking expenses...</h1>
-            </section>
-            <footer className='footer'>
-                <p>&copy; 2024 ExpenseWise. All rights reserved.</p>
-            </footer>
+            {showLogin ? (
+                <div className='login-container'>
+                    <img className='page-logo' src={page} alt="Background" />
+                    <div className='form-content'>
+                        <h2>Login</h2>
+                        <div className='form-group'>
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" placeholder="Enter your email" />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" placeholder="Enter your password" />
+                        </div>
+                        <button className='btnn'>Login</button>
+                        <a href="#" className='form-group'>Forgot Password?</a>
+                        <button className='signup' onClick={() => setShowLogin(false)}>Back</button>
+                    </div>
+                </div>
+            ) : (
+                <>
+                    <header className='header'>
+                        <a href='/'><img src={logo} alt='ExpenseWise Logo' /></a>
+                        <ul className='nav'>
+                            <li><a href='#' onClick={handleShowLogin}><i className="fas fa-sign-in-alt"></i> Login</a></li>
+                            <li><a href='/aboutus'><i className='fas fa-info-circle'></i> About Us</a></li>
+                            <li><a href='/contactus'><i className='fas fa-envelope'></i> Contact Us</a></li>
+                            <li><a href='/logout'><i className='fas fa-sign-out-alt'></i> Log Out</a></li>
+                        </ul>
+                    </header>
+                    <section className='main section'>
+                        <img className='img-bg' src={page} alt="Background" />
+                        <h1>Welcome! <br />Your reliable app for tracking expenses...</h1>
+                    </section>
+                    <footer className='footer'>
+                        <p>&copy; 2024 ExpenseWise. All rights reserved.</p>
+                    </footer>
+                </>
+            )}
         </>
     );
 }
