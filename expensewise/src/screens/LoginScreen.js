8d22344902a852666757e46b26e8c72b6df72338
Mogbolahan Folarin <mogbolahan@aol.com>
2024-10-30 13:52:53 +0100

    Login Screen Component

--- expensewise/src/screens/LoginScreen.js
+++ expensewise/src/screens/LoginScreen.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LoginScreen() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        // Add login logic here
+    };
+
     return (
         <div>
-            <h2>Login or Sign Up</h2>
-            {/* Add fields for login/signup here */}
+            <h2>Login</h2>
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleLogin}>Login</button>
+            <button>Sign in with Google</button>
+            <button>Sign in with Apple</button>
         </div>
     );
 }
