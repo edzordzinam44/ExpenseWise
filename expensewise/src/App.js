9deb4262f290aa6c41f17552fb48bfbd1810a04c
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-10-31 07:52:49 +0000

    Add dashboard route to application

--- expensewise/src/App.js
+++ expensewise/src/App.js
@@ -4,18 +4,18 @@ import Homepage from './Pages/Home/Homepage';
 import Login from './Pages/Register/Login/login';
 import SignUp from './Pages/Register/Signup/signup';
 import ForgotPass from './Pages/Register/ForgotPass/fortgotPass';
-// import Dashboard from './Pages/Dashboard';
+import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPass />} />
         <Route path='*' element={<h1>404 Not Found</h1>} />
-        {/* <Route path='/dashboard' element={<Dashboard />} /> */}
       </Routes>
     </BrowserRouter>
   );
