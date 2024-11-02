3397233f72ecdfbcd423ba405116f635a1131fdd
Hope.edzordzinam <edzordzinam.alipui@gmail.com>
2024-11-02 15:31:25 +0000

    Delete Authentication directory

--- Authentication/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
-mongoose
-  .connect("mongodb+srv://samuraxworld:NbaHpUkzHDuACdaC@cluster0.nrbni.mongodb.net/Cluster0")
-  .then(() => console.log("Database connected"))
-  .catch((err) => console.error("Database connection error", err));
