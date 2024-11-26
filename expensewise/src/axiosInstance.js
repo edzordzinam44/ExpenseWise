091c7c1de0995cf11552ac1c3eb3aa4c0188ee98
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-11-26 13:24:05 +0000

    Remove unused components and axios instance to streamline codebase

--- expensewise/src/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // Replace with your backend URL
-  withCredentials: true,            // Enables cookies if needed
-});
-
-export default axiosInstance;
